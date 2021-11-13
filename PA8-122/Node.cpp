b4a2cde9477817a468cce44cfbbc00f70e3ab061
OrionGreen <90157368+OrionGreen@users.noreply.github.com>
2021-11-12 23:46:52 -0800

    Add project files.

---
+++ PA8-122/Node.cpp
@@ -0,0 +1,34 @@
+#include "Node.h"
+Node::Node(const string& newData) {
+
+}
+
+Node::~Node() {
+
+}
+
+//setters
+void Node::setData(string& const newData) {
+
+}
+
+void Node::setpLeft(Node* const newPtr) {
+
+}
+
+void Node::setpRight(Node* const newptr) {
+
+}
+
+//getters
+string Node::getData() const {
+
+}
+
+Node*& Node::getpLeft() const {
+
+}
+
+Node*& Node::getpRight() const {
+
+}
---
+++ PA8-122/Node.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <iostream>
+#include <Windows.h>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+using std::ifstream;
+using std::ofstream;
+using std::fstream;
+using std::vector;
+using std::string;
+
+class Node
+{
+
+public:
+	Node(const string& newData);
+	virtual ~Node();
+
+	//setters
+	void setData(string& const newData);
+	void setpLeft(Node* const newPtr);
+	void setpRight(Node* const newptr);
+
+	//getters
+	string getData() const;
+	Node*& getpLeft() const;
+	Node*& getpRight() const;
+
+	virtual void printData() = 0;
+
+private:
+	string mData;
+	Node* mpLeft;
+	Node* mpRight;
+};
+
